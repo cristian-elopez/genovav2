71002a61101d08dc6c5d31315622aa5f09ebf3c9
Cristian Lopez <cristianelopez05@gmail.com>
2023-12-07 16:10:03 -0300

    iniciar proyecto

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { LayoutWrapper } from './LayoutStyles';
+
+export const Layout = ({children})=> {
+    const { pathname } = useLocation();
+    useEffect(()=> {
+        window.scrollTo(0,0)
+    }, [pathname])
+    return <LayoutWrapper> { children } </LayoutWrapper>
+}
